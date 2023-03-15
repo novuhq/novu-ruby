22d169c035fded01d174c7a39200219a817faee8
Aman Saini <aman.saini@cogoport.com>
2023-03-15 18:21:16 +0530

    test: added test for messages api

---
+++ spec/messsages_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require_relative "../lib/novu"
+
+RSpec.describe Novu::Api::Messages do
+  let(:access_token) { "1234567890" }
+  let(:client) { Novu::Client.new(access_token) }
+  let(:base_uri) { "https://api.novu.co/v1" }
+
+  describe "#message" do
+    it "returns a list of messages " do
+      response_body = {
+        _id: "message_id",
+        name: "John"
+      }.to_json
+
+      stub_request(:get, "#{base_uri}/messages")
+        .to_return(status: 200, body: response_body)
+
+      result = client.messages
+
+      expect(result.body).to eq(response_body)
+      expect(result.code).to eq(200)
+    end
+  end
+
+  describe "#delete_message" do
+    it "deletes the specified message" do
+      message_id = "12345"
+      response_body = {
+        acknowledged: true,
+        status: "deleted"
+      }.to_json
+
+      stub_request(:delete, "#{base_uri}/messages/#{message_id}")
+        .to_return(status: 200, body: response_body)
+
+      result = client.delete_message(message_id)
+
+      expect(result.body).to eq(response_body)
+      expect(result.code).to eq(200)
+    end
+  end
+end
