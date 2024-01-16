193733876b8d526fdaa74ebcf19da07ee5688e50
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-01-16 17:42:02 +0000

    comment out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,9 @@ require 'webmock/rspec'
 require_relative "../lib/novu"
 
 RSpec.configure do |config|
-  config.expect_with :rspec, :test_unit
-  config.mock_with :mocha
-  config.pattern = '**/*.spec'
+  # config.expect_with :rspec, :test_unit
+  # config.mock_with :mocha
+  # config.pattern = '**/*.spec'
   
   # # Enable flags like --only-failures and --next-failure
   # config.example_status_persistence_file_path = ".rspec_status"
