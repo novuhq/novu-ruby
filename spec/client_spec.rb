ea0a9aa65ee05b2738eba9db8c62611c862f4663
Bruce Krysiak <brucek@alum.mit.edu>
2024-03-29 20:42:12 -0700

    Test cleanup and passing specs

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,8 +5,7 @@ require_relative "../lib/novu"
 RSpec.describe Novu::Client do
 
   let(:access_token) { "1234567890" }
-  let(:idempotency_key1) { "489e2e70-50be-013c-faf9-38e85644422a" }
-  let(:idempotency_key2) { "99893-50be-5d3c-k109-8920564sd22a" }
+  let(:idempotency_key) { "489e2e70-50be-013c-faf9-38e85644422a" }
 
   let(:body) {
     {
@@ -22,14 +21,14 @@ RSpec.describe Novu::Client do
   }
 
   let(:max_retries) { 3 }
-  let(:failure_response) { TestResponse.new(code: 401) }
-  let(:success_response) { TestResponse.new(code: 201) }
+  let(:success_response) { TestResponse.new(code: 201, body: response_body) }
+  let(:failure_response) { TestResponse.new(code: 401, body: response_body) }
 
   context "with exponential retry enabled" do
     let(:client) {
       Novu::Client.new(
         access_token: access_token,
-        idempotency_key: idempotency_key1,
+        idempotency_key: idempotency_key,
         enable_retry: true,
         retry_config: {
           max_retries: max_retries,
@@ -40,7 +39,7 @@ RSpec.describe Novu::Client do
 
     context "when all retries fail" do
       before do
-        allow(Novu::Client).to receive(:post).and_return(failure_response)
+        allow(Novu::Client).to receive(:post).exactly(max_retries).and_return(failure_response)
       end
       it "throws an error" do
         expect { client.create_subscriber(body) }.to raise_error(StandardError)
@@ -49,7 +48,7 @@ RSpec.describe Novu::Client do
 
     context "as soon as a request succeeds" do
       before do
-        allow(Novu::Client).to receive(:post).and_return(success_response)
+        allow(Novu::Client).to receive(:post).at_most(2).and_return(failure_response, success_response)
       end
 
       it "should return successfully" do
@@ -60,31 +59,39 @@ RSpec.describe Novu::Client do
 
   end
 
-  it "failed request not retried" do
-    client = Novu::Client.new(access_token: access_token)
-    allow(client).to receive(:request).and_return(status: 401)
+  context "with exponential retry disabled" do
+    let(:client) { Novu::Client.new(access_token: access_token, idempotency_key: idempotency_key) }
 
-    result = client.create_subscriber(body)
-    expect(result[:status]).to eq(401)
+    it "failed requests are not retried" do
+      allow(Novu::Client).to receive(:post).exactly(1).and_return(failure_response)
+
+      result = client.create_subscriber(body)
+      expect(result[:status]).to eq(401)
+    end
+
+    it "performs idempotent request with no duplicate result" do
+      allow(client).to receive(:request).at_most(3).and_return(success_response)
+      result1 = client.create_subscriber(body)
+      result2 = client.create_subscriber(body)
+      result3 = client.create_subscriber(body)
+
+      expect(result1[:body]).to eq(response_body)
+      expect(result2[:body]).to eq(response_body)
+      expect(result3[:body]).to eq(response_body)
+    end
   end
 
-  it "performs idempotent request with no duplicate result" do
-    client = Novu::Client.new(access_token: access_token, idempotency_key: idempotency_key2)
-    allow(client).to receive(:request).at_most(max_retries).and_return(status: 201, body: response_body)
-    result1 = client.create_subscriber(body)
-    result2 = client.create_subscriber(body)
-    result3 = client.create_subscriber(body)
-
-    expect(result1[:body]).to eq(response_body)
-    expect(result2[:body]).to eq(response_body)
-    expect(result3[:body]).to eq(response_body)
-  end
 end
 
 class TestResponse
-  attr_accessor :code
+  attr_accessor :code, :body
 
-  def initialize(code)
+  def initialize(code: 201, body: "")
     @code = code
+    @body = body
+  end
+
+  def [](attr)
+    attr == :status ? code : body
   end
 end
